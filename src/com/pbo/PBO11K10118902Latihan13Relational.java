941c458bd36a1fa96b259de2e9aa0d2f84c20d3b
Agung Sepruloh <agungsepruloh1996@gmail.com>
2019-09-24 19:17:02 +0700

    Initial commit

---
+++ src/com/pbo/PBO11K10118902Latihan13Relational.java
@@ -0,0 +1,26 @@
+package com.pbo;
+
+/**
+ *
+ * # Author
+ * Nama : Agung Sepruloh
+ * Kelas : PBO11K
+ * NIM : 10118902
+ *
+ */
+
+public class PBO11K10118902Latihan13Relational {
+
+    public static void main(String[] args) {
+	    int bill1 = 10;
+	    int bill2 = 20;
+
+        System.out.println("a == b = " + (bill1 == bill2));
+        System.out.println("a != b = " + (bill1 != bill2));
+        System.out.println("a > b = " + (bill1 > bill2));
+        System.out.println("a < b = " + (bill1 < bill2));
+        System.out.println("b >= a = " + (bill2 >= bill1));
+        System.out.println("b <= a = " + (bill2 <= bill1));
+    }
+
+}
